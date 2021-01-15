d47a6e844fc05d1881527e8e873333f29c0cae25
Serhii <Beezoom.94@gmail.com>
2021-01-15 17:36:42 +0200

    Lesson-39
     Скрипти та час їх виконання. setTimeout і setInterval

--- js/script.js
+++ js/script.js
@@ -1,40 +1,60 @@
-window.addEventListener('DOMContentLoaded', () => {
+// const timerId = setTimeout(function() {    // - перший аргумент - це функція, 
+//     console.log('Hello');                  // - а другим аргументом вказуємо затримку(через скільки часу вона виконається)          
+// }, 2000);                                   // - виконається через 2 секунди (2000 мс)
 
-    const tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
 
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
+// const timerId = setTimeout(function(text) {
+//     console.log(text);
+// }, 2000, 'Hello');         // - Записали третім аргументом "Hello" який по синиаксису перейде в функцію ака запускається в середині setTimeout-а
 
-        tabs.forEach(item => {
-            item.classList.remove("tabheader__item_active");
-        });
 
-    }
+// const timerId = setTimeout(logger, 2000);   // - Першим аргументом задається готова функція яка виконається через 2с 
 
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    }
+// function logger () {
+//     console.log('text');
+// }
 
-    hideTabContent();
-    showTabContent();
 
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
+// const btn = document.querySelector('.btn');
+// let timerId,             // - Створили ГЛОБАЛЬНУ змінну timerId щоб її бачили всі функції 
+//     i = 0;    // - Змінна Ітератор (Лічильник)
 
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-        }
-    });
-});
+// btn.addEventListener('click', () => {
+//     timerId = setInterval(logger, 2000); // - Функція logger виконується завжди після кліку з інтервалом 2 сек
+// });
+
+// function logger () {
+//     if (i === 3) {                 // - Якщо і = 3, то ми запускаємо clearInterval який зупинить цей безкінечний цикл
+//         clearInterval(timerId);    // - Функція яка зупиняє setTimeout та setInterval
+//     }
+//     console.log('text');
+//     i++;                   // - Змінну і кожного разу збільшуємо на 1
+
+
+
+// let interval = setTimeout(function hi(){   // - Працює як setInterval, але завжди буде чекати відведений йому час 0,5сек
+//     console.log('Hello');
+//     interval = setTimeout(hi, 500);
+// }, 500);
+
+
+
+// const btn = document.querySelector('.btn');
+
+// function myAnimation() {
+//     const elem = document.querySelector('.box');  // - Створили змінну для квадрата 
+//     let pos = 0;                                  // - // - Створили змінну для позиції
+
+//     const id = setInterval(frame, 10);  // - Запускажмо функцію frame і вказуємо затримку 10мс
+//     function frame() {
+//         if (pos == 300) {          // - кінечне значення 300px і анімація має зупинитись
+//             clearInterval(id);    // - Зупиняємо анімацію (setInterval під змінною id) коли вона достигла 300 пікселів! 
+//         } else {
+//             pos++;
+//             elem.style.top = pos + 'px';    // - Вказуємо що при кожному кроці анімації ми будемо змінювати TOP. Кажемо що сюда приходить поточна позиція + піксель
+//             elem.style.left = pos + 'px';   // - Теж саме і для ЛІВА
+//         }
+//     }
+// }
+
+// btn.addEventListener('click', myAnimation); // - Після кліку запускається функція myAnimation
